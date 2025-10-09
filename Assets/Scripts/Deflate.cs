15982d6756054d45a5493e83e8e9feb71e6474da
luke-hanlonfitzgerald-tus23 <K00299032@student.tus.ie>
2025-10-09 15:06:04 +0100

    Deflate is functioning, working on raycast detection of walls

--- Assets/Scripts/Deflate.cs
+++ Assets/Scripts/Deflate.cs
@@ -1,65 +1,80 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static Unity.Cinemachine.InputAxisControllerBase<T>;
-using static UnityEngine.UI.Image;
 
 public class Deflate : MonoBehaviour
 {
     private CharacterController playerCol;
 
     private float ccOrigRadius;
-    private float ccNewRadius;
+    private float ccNewRadius = 0.3f;
+    public float deflateWidthScale = 0.5f;
+    private Vector3 originalScale;
 
     public bool holdToDeflate = true;
     public bool rayHit = false;
 
-    public Vector3 rayOrigin;
+    public Transform playerTransform;
+
+    private Vector3 rayOrigin;
     private Vector3 rayDirectionR;
     private Vector3 rayDirectionL;
     public float rayDistance = 1f;
     public LayerMask wallLayer;
+
     void Start()
     {
         playerCol = GetComponent<CharacterController>();
         ccOrigRadius = playerCol.radius;
         rayDirectionR = transform.right;
         rayDirectionL = -transform.right;
+        originalScale = transform.localScale;
     }
 
     void Update()
     {
-        
+        rayOrigin = playerTransform.position;
+        RayWallCheck();
     }
 
-    void OnDeflate(InputAction.CallbackContext context)
-    {
+    public void OnDeflate(InputValue value)
+    { 
         if (holdToDeflate)
         {
-            if (context.started)
+            if (value.isPressed)
             {
-                playerCol.radius = ccOrigRadius * ccNewRadius;
+                playerCol.radius = ccNewRadius;
+                transform.localScale = new Vector3(originalScale.x * deflateWidthScale, originalScale.y, originalScale.z);
+
             }
-            else if (context.canceled && rayHit == false)
+            else if (!value.isPressed && rayHit == false)
             {
                 playerCol.radius = ccOrigRadius;
-
+                transform.localScale = originalScale;
             }
         }
     }
 
-    void rayWallCheck()
+    void RayWallCheck()
     {
         if (Physics.Raycast(rayOrigin, rayDirectionR, rayDistance, wallLayer))
         {
             rayHit = true;
+            Gizmos.DrawRay(rayOrigin, rayDirectionR * rayDistance);
+            Debug.Log("Ray hit right wall");
+
         }
         if (Physics.Raycast(rayOrigin, rayDirectionL, rayDistance, wallLayer))
         {
             rayHit = true;
+            Gizmos.DrawRay(rayOrigin, rayDirectionL * rayDistance);
+            Debug.Log("Ray hit left wall");
+
         }
         else
         {
             rayHit = false;
+            Debug.Log("Ray not hitting wall");
         }
     }
 }
